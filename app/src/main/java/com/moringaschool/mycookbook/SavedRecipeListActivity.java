c18e4963701356e76eb725e68b12de9398574a35
ramzanthenolph <ramzanthenolph@gmail.com>
2020-08-17 23:26:57 +0300

    add saved list activity

---
+++ app/src/main/java/com/moringaschool/mycookbook/SavedRecipeListActivity.java
@@ -0,0 +1,13 @@
+package com.moringaschool.mycookbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedRecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
