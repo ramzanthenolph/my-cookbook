e0a8805131df458a0856642aa1e0a583563fb168
ramzanthenolph <ramzanthenolph@gmail.com>
2020-08-17 23:53:57 +0300

    add setUpFirebase adapter

--- app/src/main/java/com/moringaschool/mycookbook/SavedRecipeListActivity.java
+++ app/src/main/java/com/moringaschool/mycookbook/SavedRecipeListActivity.java
@@ -1,11 +1,17 @@
 package com.moringaschool.mycookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -29,5 +35,42 @@ public class SavedRecipeListActivity extends AppCompatActivity {
         setUpFirebaseAdapter();
     }
 
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<UsedIngredient> options =
+                new FirebaseRecyclerOptions.Builder<UsedIngredient>()
+                        .setQuery(mIngredientReference, UsedIngredient.class)
+                        .build();
 
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<UsedIngredient, FirebaseIngredientViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseIngredientViewHolder firebaseIngredientViewHolder, int i, @NonNull UsedIngredient ingredient) {
+                firebaseIngredientViewHolder.bindIngredient(ingredient);
+            }
+
+
+            @NonNull
+            @Override
+            public FirebaseIngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cookbook_list_item, parent, false);
+                return new FirebaseIngredientViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
 }
