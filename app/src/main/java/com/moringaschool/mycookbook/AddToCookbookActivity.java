040eba73329e28c81a1a6b890e34ca53df6e20ad
ramzanthenolph <ramzanthenolph@gmail.com>
2020-08-17 14:13:09 +0300

    add database reference

--- app/src/main/java/com/moringaschool/mycookbook/AddToCookbookActivity.java
+++ app/src/main/java/com/moringaschool/mycookbook/AddToCookbookActivity.java
@@ -11,18 +11,27 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddToCookbookActivity extends AppCompatActivity implements View.OnClickListener {
     //private SharedPreferences mSharedPreferences;
     //private SharedPreferences.Editor mEditor;
+    private DatabaseReference mSearchedIngredientReference;
 
     @BindView(R.id.toCookbookButton) Button mToCookbookButton;
     @BindView(R.id.ingredientEditText) EditText mIngredientEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSearchedIngredientReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_INGREDIENT);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtocookbook);
         ButterKnife.bind(this);
@@ -38,13 +47,18 @@ public class AddToCookbookActivity extends AppCompatActivity implements View.OnC
     public void onClick(View v) {
         if (v == mToCookbookButton){
             String ingredient = mIngredientEditText.getText().toString();
-
+            saveIngredientToFirebase(ingredient);
             Intent intent = new Intent(AddToCookbookActivity.this, CookbookActivity.class);
             intent.putExtra("ingredient", ingredient);
             startActivity(intent);
             Toast.makeText(AddToCookbookActivity.this, "Added successfully to Cookbook", Toast.LENGTH_LONG).show();
         }
     }
+
+    public void saveIngredientToFirebase(String ingredient) {
+        mSearchedIngredientReference.setValue(ingredient);
+    }
+
 
     /*private void addToSharedPreferences(String ingredient) {
         mEditor.putString(Constants.PREFERENCES_FOOD_KEY, ingredient).apply();
