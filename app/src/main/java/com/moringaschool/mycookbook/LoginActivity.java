f702174edb68c49022d37dca2641401830771558
ramzanthenolph <ramzanthenolph@gmail.com>
2020-08-18 19:46:45 +0300

    add on click listener to login activity

--- app/src/main/java/com/moringaschool/mycookbook/LoginActivity.java
+++ app/src/main/java/com/moringaschool/mycookbook/LoginActivity.java
@@ -2,13 +2,32 @@ package com.moringaschool.mycookbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent registerIntent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(registerIntent);
+            finish();
+        }
     }
 }
