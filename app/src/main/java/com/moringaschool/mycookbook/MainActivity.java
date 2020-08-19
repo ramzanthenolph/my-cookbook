0e51eaed162468b154c4ebc180940bb4766b9985
ramzanthenolph <ramzanthenolph@gmail.com>
2020-08-19 09:24:07 +0300

    add on create optionsmenu function

--- app/src/main/java/com/moringaschool/mycookbook/MainActivity.java
+++ app/src/main/java/com/moringaschool/mycookbook/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -34,5 +36,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent toViewIntent = new Intent(MainActivity.this, CookbookActivity.class);
             startActivity(toViewIntent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
